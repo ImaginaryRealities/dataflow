2e2a8d2ac3d595186277aec60571b6a0627493b4
Michael Collins <michael@imaginaryrealities.com>
2013-10-17 08:22:45 -0700

    Send message to transactional MSMQ queue
    I created the TransactionalMessageQueueTargetBlock<T> class and its
    unit tests. This target block will send a message to a transactional
    MSMQ message queue in a pipeline.

---
+++ src/MsmqBlockUnitTests/UnitTests/TransactionalMessageQueueTargetBlockTests.cs
@@ -0,0 +1,56 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="TransactionalMessageQueueTargetBlockTests.cs"
+//            company="ImaginaryRealities">
+// Copyright 2013 ImaginaryRealities, LLC
+// </copyright>
+//-----------------------------------------------------------------------------
+
+namespace ImaginaryRealities.Framework.Dataflow.Msmq.UnitTests
+{
+    using System;
+    using System.Messaging;
+    using System.Threading.Tasks.Dataflow;
+
+    using Moq;
+    using Moq.Protected;
+
+    using Xunit;
+
+    public class TransactionalMessageQueueTargetBlockTests
+    {
+        private readonly TransactionalMessageQueueTargetBlock<string> block;
+
+        private readonly Mock<MessageQueueFactory> mockMessageQueueFactory =
+            new Mock<MessageQueueFactory>(MockBehavior.Strict);
+
+        private readonly Mock<TransactionService> mocktransactionService =
+            new Mock<TransactionService>(MockBehavior.Strict); 
+
+        public TransactionalMessageQueueTargetBlockTests()
+        {
+            this.block = new TransactionalMessageQueueTargetBlock<string>(
+                this.mocktransactionService.Object,
+                this.mockMessageQueueFactory.Object,
+                @".\private$\test");
+        }
+
+        [Fact]
+        public void BlockSendsMessageToTransactionalQueue()
+        {
+            var mockTransactionScope = new Mock<TransactionScopeBase>(MockBehavior.Strict);
+            mockTransactionScope.Protected().Setup("Dispose", true);
+            this.mocktransactionService.Setup(ts => ts.CreateTransactionScope()).Returns(mockTransactionScope.Object);
+            mockTransactionScope.Setup(ts => ts.Complete()).Verifiable();
+            var mockMessageQueue = new Mock<MessageQueueBase>(MockBehavior.Strict);
+            mockMessageQueue.Protected().Setup("Dispose", true);
+            this.mockMessageQueueFactory.Setup(f => f.CreateMessageQueue(@".\private$\test", QueueAccessMode.Send))
+                .Returns(mockMessageQueue.Object);
+            mockMessageQueue.Setup(q => q.Send("Hello!", MessageQueueTransactionType.Automatic)).Verifiable();
+            Assert.True(this.block.Post("Hello!"));
+            this.block.Complete();
+            Assert.True(this.block.Completion.Wait(TimeSpan.FromSeconds(30.0)));
+            mockTransactionScope.Verify(ts => ts.Complete(), Times.Once());
+            mockMessageQueue.Verify(q => q.Send("Hello!", MessageQueueTransactionType.Automatic), Times.Once());
+        }
+    }
+}
---
+++ src/MsmqBlocks/TransactionalMessageQueueTargetBlock.cs
@@ -0,0 +1,102 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="TransactionalMessageQueueTargetBlock.cs" 
+//            company="ImaginaryRealities">
+// Copyright 2013 ImaginaryRealities, LLC
+// </copyright>
+//-----------------------------------------------------------------------------
+
+namespace ImaginaryRealities.Framework.Dataflow.Msmq
+{
+    using System;
+    using System.Messaging;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+
+    /// <summary>
+    /// Dataflow target block that will send messages to a transactional MSMQ
+    /// queue.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type for the body of the message.
+    /// </typeparam>
+    public sealed class TransactionalMessageQueueTargetBlock<T> : ITargetBlock<T>
+    {
+        private readonly ITargetBlock<T> innerTargetBlock;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransactionalMessageQueueTargetBlock{T}"/> class.
+        /// </summary>
+        /// <param name="path">
+        /// The path of the message queue to send messages to.
+        /// </param>
+        public TransactionalMessageQueueTargetBlock(string path)
+            : this(new InternalTransactionService(), new InternalMessageQueueFactory(), path)
+        {
+        }
+
+        internal TransactionalMessageQueueTargetBlock(
+            TransactionService transactionService,
+            MessageQueueFactory messageQueueFactory,
+            string path)
+        {
+            this.innerTargetBlock = new ActionBlock<T>(
+                m =>
+                    {
+                        using (var transactionScope = transactionService.CreateTransactionScope())
+                        {
+                            var messageQueue = messageQueueFactory.CreateMessageQueue(path, QueueAccessMode.Send);
+                            using (messageQueue)
+                            {
+                                messageQueue.Send(m, MessageQueueTransactionType.Automatic);
+                            }
+
+                            transactionScope.Complete();
+                        }
+                    });
+        }
+
+        /// <summary>
+        /// Gets a <see cref="Task"/> object that represents the asynchronous
+        /// operation and completion of the dataflow block.
+        /// </summary>
+        /// <value>
+        /// A <see cref="Task"/> object.
+        /// </value>
+        public Task Completion
+        {
+            get
+            {
+                return this.innerTargetBlock.Completion;
+            }
+        }
+
+        /// <summary>
+        /// Signals to the dataflow block that it should not accept nor
+        /// produce any more messages nor consume any more postponed messages.
+        /// </summary>
+        public void Complete()
+        {
+            this.innerTargetBlock.Complete();
+        }
+
+        /// <summary>
+        /// Causes the dataflow block to complete in a faulted state.
+        /// </summary>
+        /// <param name="exception">
+        /// The exception that caused the fault.
+        /// </param>
+        public void Fault(Exception exception)
+        {
+            this.innerTargetBlock.Fault(exception);
+        }
+
+        DataflowMessageStatus ITargetBlock<T>.OfferMessage(
+            DataflowMessageHeader messageHeader,
+            T messageValue,
+            ISourceBlock<T> source,
+            bool consumeToAccept)
+        {
+            return this.innerTargetBlock.OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+        }
+    }
+}
